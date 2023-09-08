c6fb4b2c7fad1a361d258fb464c300869270a6bb
windlil <windlil@outlook.com>
2023-09-08 22:33:52 +0800

    feat: new component

---
+++ src/components/layout/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Toggle from '@/components/toggle/index.vue'
+</script>
+
+<template>
+  <div class="layout">
+    <div class="sider">
+      <Toggle />
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/components/toggle/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { Icon } from '@iconify/vue'
+
+const htmlEl = document.documentElement
+const toggleRef = ref()
+
+const isDark = ref(false)
+
+onMounted(() => {
+  toggleRef.value.addEventListener('click', () => {
+    const dataTheme = htmlEl.getAttribute('data-theme')
+    if (dataTheme) {
+      htmlEl.removeAttribute('data-theme')
+    } else {
+      htmlEl.setAttribute('data-theme', 'dark')
+    }
+    isDark.value = !isDark.value
+  })
+})
+</script>
+
+<template>
+  <div ref="toggleRef" class="toggle">
+    <Icon v-if="isDark" class="toggleIcon" icon="eva:sun-outline" />
+    <Icon v-else class="toggleIcon" icon="eva:moon-outline" />
+  </div>
+</template>
+
+<style scoped lang="less">
+.toggleIcon {
+  font-size: 35px;
+  color: var(--color-icon-toggle);
+  cursor: pointer;
+}
+</style>
